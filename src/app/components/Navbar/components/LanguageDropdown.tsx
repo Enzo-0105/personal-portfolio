3c468007ab09acaaab2b87a66f80efd99031f39b
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2024-05-27 06:16:48 +0100

    Use svg image instead of application icon

--- src/app/components/Navbar/components/LanguageDropdown.tsx
+++ src/app/components/Navbar/components/LanguageDropdown.tsx
@@ -1,30 +1,40 @@
 "use client";
 import React, { useState } from "react";
 import styles from "../styles/languagedropdown.module.css";
-import { ChevronDownOutline } from "react-ionicons";
+import Image from "next/image";
 
 const LanguageDropdown = () => {
   const [open, setOpen] = useState(false);
   return (
     <div className={styles["dropdown"]}>
-      <button onClick={() => setOpen(!open)} className="form-btn m-auto" type="submit" data-form-btn>
+      <button
+        onClick={() => setOpen(!open)}
+        className="form-btn m-auto"
+        type="submit"
+        data-form-btn
+      >
         <span className="fi fi-us"></span>{" "}
-        <ChevronDownOutline
-          color={"#00000"}
-          //   title={}
-          height="18px"
-          width="18px"
+        <Image
+          alt="chevron"
+          width={20}
+          height={20}
+          style={{ color: "var(--orange-yellow-crayola)" }}
+          src="/assets/images/svg-icons/chevron-down-outline.svg"
         />
       </button>
       <div
         className={`${styles["dropdown-content"]}`}
         style={{ display: open ? "block" : "none" }}
       >
-        <a className={`${styles["selected"]}`} style={{color: "black"}} href="#">
+        <a
+          className={`${styles["selected"]}`}
+          style={{ color: "black" }}
+          href="#"
+        >
           <span className="fi fi-us"></span>
           <span>EN-English</span>
         </a>
-        <a  href="#">
+        <a href="#">
           <span className="fi fi-fr"></span>
           <span>(Bientôt!)</span>
         </a>
